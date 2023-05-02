dcf25f05f6c37ef5af7168c1e2e90c5f022250a6
Lucas Álvares <lucas.alvaress@hotmail.com>
2023-05-02 10:48:48 -0300

    🧱 basic structure

---
+++ src/app/(login)/login/layout.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Box, Container, Typography } from '@mui/material';
+
+type Props = {
+    children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+    return (
+        <html>
+            <body>
+                <Container component='main' maxWidth='xs'>
+                    <Box sx={{
+                        mt: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center'
+                    }}>
+                        <Typography component='h3' variant='h3'>NextDelivery</Typography>
+                        <Typography component='h3' variant='h3'>Admin Panel</Typography>
+                        {children}
+                    </Box>
+                </Container>
+            </body>
+        </html>
+    );
+}
+
+export default Layout
