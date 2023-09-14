12e2207ac612fc4ed11cb651c4a436df81cce541
Lucas Álvares <lucas.alvaress@hotmail.com>
2023-09-14 09:17:40 -0300

    ✨ create inside basic layout

---
+++ src/app/(inside)/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Container } from "@mui/material";
+import { Header } from "@/components/header";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+    return (
+        <html>
+            <body style={{ margin: 0 }}>
+                <Header />
+                <Container component="section" maxWidth="lg">
+                    {children}
+                </Container>
+            </body>
+        </html>
+    );
+};
+
+export default Layout;
---
+++ src/app/(inside)/orders/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Box } from "@mui/material";
+
+const Page = () => {
+    return <Box>Oi</Box>;
+};
+
+export default Page;
